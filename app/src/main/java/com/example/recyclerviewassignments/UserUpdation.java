463a8a43bdd862c2f269de4c888f01f224b1ab80
Shivam Sharma <shivam.sharma@berylsystems.com>
2021-03-04 18:32:29 +0530

    Delete function and add function is updated

--- app/src/main/java/com/example/recyclerviewassignments/UserUpdation.java
+++ app/src/main/java/com/example/recyclerviewassignments/UserUpdation.java
@@ -2,13 +2,26 @@ package com.example.recyclerviewassignments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class UserUpdation extends AppCompatActivity {
-
+   EditText usernaemupdate_;
+   Button update_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_updation);
+
+    usernaemupdate_ =findViewById(R.id.username_update);
+    update_btn = findViewById(R.id.updatebutton);
+
+  String passname = usernaemupdate_.getText().toString();
+
+        Intent intent = new Intent(UserUpdation.this,MainActivity.class);
+          intent.putExtra("username",passname);
     }
 }
